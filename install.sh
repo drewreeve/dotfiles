8eb45f29c163324c78ea86dd754e8905bcfab13a
Drew <dru.reeve@gmail.com>
2016-05-10 12:06:04 +0100

    Optionally set extra settings to enable truecolor
    Choosing yes when prompted by install.sh will set additional settings
    in tmux.conf.local and vimrc.local to enable truecolor in tmux and
    neovim
    
    Terminals supporting truecolor are listed here:
    https://gist.github.com/XVilka/8346728

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ for file in "$dotfiles_dir"/*; do
 done
 
 # Allow nvim to use vim files/folder
+mkdir -p $HOME/.config
 ln -sfnv $HOME/.vim $HOME/.config/nvim
 ln -sfnv $HOME/.vimrc $HOME/.config/nvim/init.vim
 
@@ -30,5 +31,26 @@ if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
   curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+# Does this machine support truecolor?
+_append_truecolor_config() {
+  # Enable true color for neovim
+  echo 'if hash("nvim")' >> $HOME/.vimrc.local
+  echo '  let $NVIM_TUI_ENABLE_TRUE_COLOR=1' >> $HOME/.vimrc.local
+  echo 'endif' >> $HOME/.vimrc.local
+
+  # Enable truecolor in tmux 2.2+
+  echo 'set-option -ga terminal-overrides ",xterm-256color:Tc"' >> $HOME/.tmux.conf.local
+}
+
+while true; do
+  read "?Does this machine support truecolor in terminal? " yn
+  case $yn in
+    [Yy]* ) _append_truecolor_config; break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+
 
 vim -u $HOME/.vimrc.bundles +PlugInstall +PlugClean! +qa
