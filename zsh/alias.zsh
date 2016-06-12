fce15f1c17ec00b342d36da79c138729c287c69f
Drew Reeve <dru.reeve@gmail.com>
2016-06-12 09:54:42 +0100

    Remove unused aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -22,21 +22,5 @@ alias ppath='echo $PATH | tr -s ":" "\n"'
 # Grep
 alias grep='grep --color=auto'
 
-# Resource use
-if (( $+commands[htop] )); then
-  alias top=htop
-else
-  if [[ "$OSTYPE" == (darwin*|*bsd*) ]]; then
-    alias topc='top -o cpu'
-    alias topm='top -o vsize'
-  else
-    alias topc='top -o %CPU'
-    alias topm='top -o %MEM'
-  fi
-fi
-
-# List ten most used commands
-alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
-
 # Load machine specific aliases
 [[ -f ~/.aliases.local ]] && source ~/.aliases.local
