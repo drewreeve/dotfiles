01df591b0c207a32126b13ac246462705b350a23
Drew Reeve <dru.reeve@gmail.com>
2016-07-02 13:27:12 +0100

    Fix binstubs path being overridden by chruby
    chruby uses a preexec hook to handle automatically switching rubies.
    This change adds our own preexec hook which runs after chruby to switch
    the paths back.

--- zsh/configs/post/trusted_paths.zsh
+++ zsh/configs/post/trusted_paths.zsh
@@ -1,8 +1,22 @@
 # Prepend node and bundler binstubs to path in trusted repositories
 # Use git trust or mkdir .git/safe to mark a repository as trusted
 
-path[1,0]=.git/safe/../../node_modules/.bin
-path[1,0]=.git/safe/../../bin
+function _add_trusted_local_bin_to_path() {
+  path[1,0]=.git/safe/../../node_modules/.bin
+  path[1,0]=.git/safe/../../bin
+}
+
+_add_trusted_local_bin_to_path
 
 # Complete trusted binstubs
 zstyle -e ':completion:*' command-path '[[ -d $PWD/.git/safe/../../bin ]] && reply=($PWD/bin $PATH)'
+
+# If we're using chruby we have to redo setting this path since changing
+# rubies will place the new ruby path before our binstubs
+
+
+if (($+commands[chruby-exec])); then
+  if [[ ! "$preexec_functions" == *_add_trusted_local_bin_to_path* ]]; then
+    preexec_functions+=("_add_trusted_local_bin_to_path")
+  fi
+fi
