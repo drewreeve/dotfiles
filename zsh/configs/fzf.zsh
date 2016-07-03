4d37a4bf166ec8411b85840d3e70666be0975b08
Drew Reeve <dru.reeve@gmail.com>
2016-07-03 12:56:51 +0100

    Use provided .fzf.zsh shell file to setup path etc

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -1,3 +1,10 @@
-# Add fzf to path
-path+=~/.fzf/bin
-path=($^path(N))
+#
+# Setup fzf
+#
+
+[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+
+# Use ag for default command if it's available
+if (($+commands[ag])); then
+  export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+fi
