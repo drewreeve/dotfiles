0e60d0ead098459ef658e1fa2870e69c7509b4cf
Drew Reeve <dru.reeve@gmail.com>
2016-07-08 12:33:30 +0100

    Add some cool cd movements

--- zsh/configs/options.zsh
+++ zsh/configs/options.zsh
@@ -2,6 +2,9 @@
 # Zsh options/settings
 #
 
+setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
+DIRSTACKSIZE=5
+
 # Magic urls
 autoload -Uz url-quote-magic
 zle -N self-insert url-quote-magic
