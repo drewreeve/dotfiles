37ea8cb628f4bd4ecbb200b035f809582404e895
Drew Reeve <dru.reeve@gmail.com>
2017-07-16 16:00:49 +0100

    Remove references to diff-highlight

--- install.sh
+++ install.sh
@@ -32,7 +32,4 @@ if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# Download diff-highlight script
-curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight > $HOME/bin/diff-highlight && chmod +x $HOME/bin/diff-highlight
-
 vim +PlugInstall +PlugClean! +qa
