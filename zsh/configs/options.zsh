7314692a4d583f2cbb9368e59f511c518739519f
Drew Reeve <dru.reeve@gmail.com>
2017-09-22 18:34:53 +0100

    Remove autocd and cdablevars
    Tired of random directories showing up when tab completing, or
    changing directory when running a script

--- zsh/configs/options.zsh
+++ zsh/configs/options.zsh
@@ -2,7 +2,12 @@
 # Zsh options/settings
 #
 
-setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
+setopt AUTO_PUSHD           # Push old directory onto stack on cd
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack
+setopt PUSHD_SILENT         # Do not print directory stack after pushd/popd
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given
+setopt PUSHD_MINUS
+
 DIRSTACKSIZE=5
 
 # Magic urls
