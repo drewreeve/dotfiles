615d51c19cac657d12cbebff38ae802e8db49cdd
Drew Reeve <dru.reeve@gmail.com>
2017-10-03 15:06:02 +0100

    Use zsh-autosuggestions

---
+++ zsh/.zsh/configs/autosuggestions.zsh
@@ -0,0 +1,6 @@
+# Source zsh-autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+
+if [ -f ~/.zsh/autosuggestions/zsh-autosuggestions.zsh ]; then
+  source ~/.zsh/autosuggestions/zsh-autosuggestions.zsh
+fi
