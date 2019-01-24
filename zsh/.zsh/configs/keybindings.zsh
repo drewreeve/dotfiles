5166f8cfa97418452a62b93db80acf7c50f55c98
Drew Reeve <dru.reeve@gmail.com>
2019-01-24 11:41:08 +0000

    alacritty + zsh: keybind fixes
    This fixes some problems I was having where different keys in different
    terminals didn't actually do anything but output ~.

--- zsh/.zsh/configs/keybindings.zsh
+++ zsh/.zsh/configs/keybindings.zsh
@@ -1,8 +1,77 @@
+# Borrowed from zim
+# https://github.com/zimfw/zimfw/blob/master/modules/input/init.zsh
+
+# Return if requirements are not found.
+if [[ ${TERM} == 'dumb' ]]; then
+  return 1
+fi
+
 bindkey -e
 
-# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey "^[[3~" delete-char
-bindkey "^K" kill-line
+# Use human-friendly identifiers.
+zmodload -F zsh/terminfo +b:echoti +p:terminfo
+typeset -gA key_info
+key_info=(
+  'Control'      '\C-'
+  'ControlLeft'  '\e[1;5D \e[5D \e\e[D \eOd \eOD'
+  'ControlRight' '\e[1;5C \e[5C \e\e[C \eOc \eOC'
+  'Escape'       '\e'
+  'Meta'         '\M-'
+  'Backspace'    "${terminfo[kbs]}"
+  'BackTab'      "${terminfo[kcbt]}"
+  'Left'         "${terminfo[kcub1]}"
+  'Down'         "${terminfo[kcud1]}"
+  'Right'        "${terminfo[kcuf1]}"
+  'Up'           "${terminfo[kcuu1]}"
+  'Delete'       "${terminfo[kdch1]}"
+  'End'          "${terminfo[kend]}"
+  'F1'           "${terminfo[kf1]}"
+  'F2'           "${terminfo[kf2]}"
+  'F3'           "${terminfo[kf3]}"
+  'F4'           "${terminfo[kf4]}"
+  'F5'           "${terminfo[kf5]}"
+  'F6'           "${terminfo[kf6]}"
+  'F7'           "${terminfo[kf7]}"
+  'F8'           "${terminfo[kf8]}"
+  'F9'           "${terminfo[kf9]}"
+  'F10'          "${terminfo[kf10]}"
+  'F11'          "${terminfo[kf11]}"
+  'F12'          "${terminfo[kf12]}"
+  'Home'         "${terminfo[khome]}"
+  'Insert'       "${terminfo[kich1]}"
+  'PageDown'     "${terminfo[knp]}"
+  'PageUp'       "${terminfo[kpp]}"
+)
 
-# ctrl+t to find file and copy path
-bindkey "^T" _fzf-file-widget
+# Bind the keys
+
+local key
+for key (${(s: :)key_info[ControlLeft]}) bindkey ${key} backward-word
+for key (${(s: :)key_info[ControlRight]}) bindkey ${key} forward-word
+
+[[ -n ${key_info[Home]} ]] && bindkey ${key_info[Home]} beginning-of-line
+[[ -n ${key_info[End]} ]] && bindkey ${key_info[End]} end-of-line
+
+[[ -n ${key_info[PageUp]} ]] && bindkey ${key_info[PageUp]} up-line-or-history
+[[ -n ${key_info[PageDown]} ]] && bindkey ${key_info[PageDown]} down-line-or-history
+
+[[ -n ${key_info[Insert]} ]] && bindkey ${key_info[Insert]} overwrite-mode
+
+[[ -n ${key_info[Backspace]} ]] && bindkey ${key_info[Backspace]} backward-delete-char
+[[ -n ${key_info[Delete]} ]] && bindkey ${key_info[Delete]} delete-char
+
+[[ -n ${key_info[Left]} ]] && bindkey ${key_info[Left]} backward-char
+[[ -n ${key_info[Right]} ]] && bindkey ${key_info[Right]} forward-char
+
+# Clear
+bindkey "${key_info[Control]}L" clear-screen
+
+# Put into application mode and validate ${terminfo}
+zle-line-init() {
+  (( ${+terminfo[smkx]} )) && echoti smkx
+}
+zle-line-finish() {
+  (( ${+terminfo[rmkx]} )) && echoti rmkx
+}
+zle -N zle-line-init
+zle -N zle-line-finish
