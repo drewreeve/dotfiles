3042289311ded88d48d67d0fe13eb84f99d3bc35
Drew Reeve <dru.reeve@gmail.com>
2019-01-25 15:27:19 +0000

    autosuggestions: Update submodule to v0.5.0

--- zsh/.zsh/configs/autosuggestions.zsh
+++ zsh/.zsh/configs/autosuggestions.zsh
@@ -1,6 +1,14 @@
 # Source zsh-autosuggestions
 # https://github.com/zsh-users/zsh-autosuggestions
 
+pkgpath="/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+# Prefer arch package if it's available
+if [[ -r $pkgpath ]]; then
+  source $pkgpath
+  return
+fi
+
 if [ -f ~/.zsh/autosuggestions/zsh-autosuggestions.zsh ]; then
   source ~/.zsh/autosuggestions/zsh-autosuggestions.zsh
 fi
