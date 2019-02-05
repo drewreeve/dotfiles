00f032395294ee4f0e616b7f250d0b0dd4c3818c
Drew Reeve <dru.reeve@gmail.com>
2019-02-05 17:09:16 +0000

    zsh: Set FZF_CTRL_T_COMMAND for fzf-file-widget

--- zsh/.zsh/configs/fzf.zsh
+++ zsh/.zsh/configs/fzf.zsh
@@ -8,6 +8,8 @@ if (($+commands[fzf])); then
   # Use fd for default command if it's available
   if (($+commands[fd])); then
     export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow'
+    # Needs to be set for ctrl+t file search
+    export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
     return
   fi
 
