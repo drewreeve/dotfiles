c1678f9f45c2a56cbe34f320e97a15ace17faabd
Drew Reeve <dru.reeve@gmail.com>
2019-02-13 16:38:11 +0000

    zsh: Remove unused chruby config

--- zsh/.zsh/configs/trusted_paths.zsh
+++ zsh/.zsh/configs/trusted_paths.zsh
@@ -10,13 +10,3 @@ _add_trusted_local_bin_to_path
 
 # Complete trusted binstubs
 zstyle -e ':completion:*' command-path '[[ -d $PWD/.git/safe/../../bin ]] && reply=($PWD/bin $PATH)'
-
-# If we're using chruby we have to redo setting this path since changing
-# rubies will place the new ruby path before our binstubs
-
-
-if (($+commands[chruby-exec])); then
-  if [[ ! "$preexec_functions" == *_add_trusted_local_bin_to_path* ]]; then
-    preexec_functions+=("_add_trusted_local_bin_to_path")
-  fi
-fi
