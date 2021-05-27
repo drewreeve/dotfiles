a95c28df2cd766195ace1feda526ec94f32bf7c8
Drew Reeve <dru.reeve@gmail.com>
2021-05-27 19:02:53 +0100

    Add a very crude script to install dependencies

---
+++ fedora_setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+install_dependencies() {
+  sudo dnf install \
+    alacritty \
+    bat \
+    ripgrep \
+    stow \
+    mako \
+    grim \
+    vim \
+    sway \
+    swaylock \
+    swayidle \
+    waybar \
+    wlsunset \
+    wofi \
+    zsh \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting
+}
+
+# https://wiki.archlinux.org/title/GNOME/Keyring#Start_SSH_and_Secrets_components_of_keyring_daemon
+setup_keyring_autostart() {
+  mkdir -p ~/.config/autostart
+
+  if ! [[ -f ~/.config/autostart/gnome-keyring-secrets.desktop ]]; then
+    cp -v /etc/xdg/autostart/gnome-keyring-secrets.desktop ~/.config/autostart
+    sed -i /OnlyShowIn/d ~/.config/autostart/gnome-keyring-secrets.desktop
+  fi
+
+  if ! [[ -f ~/.config/autostart/gnome-keyring-ssh.desktop ]]; then
+    cp -v /etc/xdg/autostart/gnome-keyring-ssh.desktop ~/.config/autostart
+    sed -i /OnlyShowIn/d ~/.config/autostart/gnome-keyring-ssh.desktop
+  fi
+}
+
+install_dependencies
+setup_keyring_autostart
