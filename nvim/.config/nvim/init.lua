fc40c01147e092023beab0fd7aaca13e136e2c18
Drew Reeve <dru.reeve@gmail.com>
2022-01-18 13:50:41 +0000

    nvim: Use fzf extension with telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -126,7 +126,7 @@ require('telescope').setup{
   }
 }
 
-require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('fzf')
 
 map('n', '<C-p>',
   "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>",
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -29,7 +29,7 @@ require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = {'nvim-lua/plenary.nvim'}
   }
-  use 'nvim-telescope/telescope-fzy-native.nvim'
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Tpope
   use 'tpope/vim-commentary'
