7262ecc4fb4d28373bf75b143f18cf0a4f66d446
Drew Reeve <dru.reeve@gmail.com>
2023-07-04 12:32:40 +0100

    nvim: Auto install missing treesitter parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup {
+        auto_install = true,
         highlight = {
           enable = true
         },
