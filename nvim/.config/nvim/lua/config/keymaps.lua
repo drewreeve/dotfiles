4104c72b72d0f4f5a3920a07f3f6ae2c1eb30aee
Drew Reeve <dru.reeve@gmail.com>
2023-07-04 10:26:43 +0100

    nvim: Disable omnifunc since cmp is being used
    Per the recommendation here https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
