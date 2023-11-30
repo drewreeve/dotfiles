a8accc6ff9ee5a01682287e7f1bfc7ad8d37a7e5
Drew Reeve <dru.reeve@gmail.com>
2023-11-30 15:49:33 +0000

    nvim: Try out conform.nvim for formatting

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,31 +1,21 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  "stevearc/conform.nvim",
   dependencies = { "mason.nvim" },
-  opts = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    return {
-      debug = false,
-      sources = {
-        null_ls.builtins.formatting.stylua,
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      sh = { "shfmt" },
+    },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
       },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({
-            group = augroup,
-            buffer = bufnr,
-          })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ buffer = bufnr })
-            end,
-          })
-        end
-      end,
-    }
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
 }
