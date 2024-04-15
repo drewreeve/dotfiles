a769f3b2700ced60bed04c01f4473c32eacb8c98
Drew Reeve <dru.reeve@gmail.com>
2024-04-15 17:07:36 +0100

    nvim: Fix build function for luasnip

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -7,9 +7,17 @@ return {
       {
         -- Adds a number of user-friendly snippets
         "L3MON4D3/LuaSnip",
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+            return
+          end
+          return "make install_jsregexp"
+        end)(),
         dependencies = {
           "rafamadriz/friendly-snippets",
-          build = { "make install_jsregexp" },
           opts = { history = true, updateevents = "TextChanged,TextChangedI" },
           config = function(_, opts)
             require("luasnip.loaders.from_vscode").lazy_load(opts)
