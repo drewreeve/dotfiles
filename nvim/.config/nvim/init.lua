a51834849609c4bc41db731e97712b962b9a7ede
Drew Reeve <dru.reeve@gmail.com>
2024-05-17 12:27:12 +0100

    nvim: Update bootstrap code for lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,7 @@ require("config.options")
 require("config.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
