1482a56665c00437b282c286be07ba248e53c220
Drew Reeve <dru.reeve@gmail.com>
2024-05-21 19:08:47 +0100

    nvim: Add some diagnostic keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,14 +2,24 @@
 -- Global Mappings
 -- ===========================================================================
 
+local set = vim.keymap.set
+
 -- Clear highlighted searches
-vim.keymap.set("n", "<leader>c", ":nohlsearch<CR>", { silent = true })
+set("n", "<leader>c", ":nohlsearch<CR>", { silent = true })
 
 -- Switch between last 2 files
-vim.keymap.set("n", "<leader><leader>", "<C-^>")
+set("n", "<leader><leader>", "<C-^>")
 
 -- Move around splits with <c-hjkl>
-vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+set("n", "<C-h>", "<C-w>h", { silent = true })
+set("n", "<C-j>", "<C-w>j", { silent = true })
+set("n", "<C-k>", "<C-w>k", { silent = true })
+set("n", "<C-l>", "<C-w>l", { silent = true })
+
+-- Diagnostics
+set("n", "<leader>sd", vim.diagnostic.open_float, { desc = "[S]how [D]iagnostics" })
+
+-- These are mapped by default in neovim 0.10 but for some reason they don't
+-- show the diagnostic float when you use them.
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
