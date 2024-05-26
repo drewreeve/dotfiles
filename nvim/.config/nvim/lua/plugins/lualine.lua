76251bc24f9cbb07a62478ac5214739a4ef53306
Drew Reeve <dru.reeve@gmail.com>
2024-05-26 17:57:11 +0100

    nvim: Tweak lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,24 +2,21 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "catppuccin",
+    opts = {
+      -- options = {
+      --   theme = "catppuccin",
+      -- },
+      sections = {
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = {
+          { "filename", path = 1 },
         },
-        sections = {
-          lualine_c = {
-            { "filename", path = 1 },
-          },
-          lualine_x = {
-            -- "encoding",
-            -- { "fileformat", icons_enabled = false },
-            "filetype",
-          },
+        lualine_x = {
+          -- "encoding",
+          -- { "fileformat", icons_enabled = false },
+          "filetype",
         },
-      })
-
-      require("lualine").get_config()
-    end,
+      },
+    },
   },
 }
