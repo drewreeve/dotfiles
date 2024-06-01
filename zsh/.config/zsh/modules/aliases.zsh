1e04852eed2eb4b566d577b9e02ffb1f58c05761
Drew Reeve <dru.reeve@gmail.com>
2024-06-01 15:38:13 +0100

    zsh: Add alias to read/write ansible variables

--- zsh/.config/zsh/modules/aliases.zsh
+++ zsh/.config/zsh/modules/aliases.zsh
@@ -30,5 +30,36 @@ if (($+commands[bat])); then
   alias cat='bat'
 fi
 
+# Read/write variable from ansible playbook
+avar() {
+  __avar_error() {
+    \cat >&2 <<'EOF'
+Usage: avar [action] [variable name]
+
+Actions:
+  r, read       Read value of variable
+  e, encrypt    Encrypt stdin string as variable
+
+Example:
+  avar r secret    Print the value of the 'secret' variable
+  avar e secret    Encrypt string from stdin and name it 'secret'
+EOF
+  }
+
+  if [[ -v 1 ]] && [[ -v 2 ]]; then
+    case "$1" in
+      r | read )
+        ansible localhost -m debug -a var="$2";;
+      e | encrypt )
+       ansible-vault encrypt_string  --stdin-name "$2";;
+      *)
+        __avar_error
+    esac
+  else
+    __avar_error
+  fi
+  unset -f __avar_error
+}
+
 # Load machine specific aliases
 [[ -f ~/.aliases.local ]] && source ~/.aliases.local
