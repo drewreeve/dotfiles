9c57b90fec34f0fd3df47239a39faf6fe01255ff
Drew Reeve <dru.reeve@gmail.com>
2026-01-25 12:41:39 +0000

    fish: Allow for additional machine specific config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,8 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
 
 end
+
+# Source local machine-specific configuration if it exists
+if test -f ~/.local.fish
+  source ~/.local.fish
+end
