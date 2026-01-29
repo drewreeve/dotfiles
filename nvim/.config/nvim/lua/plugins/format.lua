276be8e1c2cca9c75574de569cba202350c61dab
Drew Reeve <dru.reeve@gmail.com>
2026-01-29 16:29:17 +0000

    nvim: Add python lsp/linter/formatter

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -5,6 +5,7 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
+      python = { "ruff_format" },
       sh = { "shfmt" },
     },
     formatters = {
---
+++ nvim/.config/nvim/lua/plugins/lang/python.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      settings = {
+        servers = {
+          ty = {},
+          ruff = {},
+        },
+      },
+    },
+  },
+}
