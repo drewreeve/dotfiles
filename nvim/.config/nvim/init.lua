888162b1803bfaa61d68450f98bc631312384653
Drew Reeve <dru.reeve@gmail.com>
2026-03-04 15:47:08 +0000

    nvim: Use virtual_lines for current line diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -67,7 +67,6 @@ opt.confirm = true
 -- Diagnostic Config
 -- See :help vim.diagnostic.Opts
 --
-
 vim.diagnostic.config({
   severity_sort = true,
   float = { border = "rounded", source = "if_many" },
@@ -81,16 +80,12 @@ vim.diagnostic.config({
     },
   } or {},
   virtual_text = {
-    source = "if_many",
-    spacing = 2,
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
+    current_line = false,
   },
+  virtual_lines = {
+    current_line = true,
+  },
+
+  -- Don't update diagnostics when typing
+  update_in_insert = false,
 })
