582150fa6fa5dbe36733a652309fc11b76ced34b
Drew Reeve <dru.reeve@gmail.com>
2026-03-08 17:19:22 +0000

    fish: More abbreviations

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -1,12 +1,19 @@
 abbr -a e $EDITOR
 
 if type -q bat
-  abbr -a cat bat
+    abbr -a cat bat
 end
 
 # Ansible
-abbr -a ave --set-cursor ansible-vault encrypt_string --stdin-name "\"%\"" 
+abbr -a ave --set-cursor ansible-vault encrypt_string --stdin-name "\"%\""
 abbr -a avr --set-cursor ansible localhost -m debug -a var="\"%\""
+
+# Git
+abbr -a g git
+abbr -a gc git commit
+abbr -a gp git push
+abbr -a gpl git push --force-with-lease
+abbr -a gd git diff
 
 # Mise
 abbr -a mr --set-cursor mise run %
