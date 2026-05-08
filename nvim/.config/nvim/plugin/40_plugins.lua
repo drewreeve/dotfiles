a57de5364739faf5a4f786f33f4c5c3f03cb8350
Drew Reeve <dru.reeve@gmail.com>
2026-05-08 12:53:16 +0100

    Migrate LSP/linter management from mason/homebrew to mise

--- nvim/.config/nvim/plugin/40_plugins.lua
+++ nvim/.config/nvim/plugin/40_plugins.lua
@@ -47,30 +47,18 @@ end)
 
 now_if_args(function()
   add({
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/mason-org/mason-lspconfig.nvim",
-    "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/b0o/schemastore.nvim",
   })
 
-  require("mason").setup()
-  require("mason-lspconfig").setup()
-
-  require("mason-tool-installer").setup({
-    ensure_installed = {
-      "ansiblels",
-      "bashls",
-      "jsonls",
-      "lua_ls",
-      "pyright",
-      "ruff",
-      "shellcheck",
-      "shfmt",
-      "stylua",
-      -- "ty",
-      "yamlls",
-    },
+  vim.lsp.enable({
+    "ansiblels",
+    "bashls",
+    "jsonls",
+    "lua_ls",
+    "pyright",
+    "ruff",
+    "yamlls",
   })
 end)
 
